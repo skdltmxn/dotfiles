8919c2bca3f3c42b3a3eae9e02f10eee2382e99d
gd-park <gd.park@overdare.com>
2025-03-27 11:53:50 +0900

    feat: add auto completion

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -16,6 +16,11 @@ return {
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				if client:supports_method("textDocument/completion") then
+					vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+				end
+
 				local opts = { buffer = ev.buf, silent = true }
 
 				ks("n", "[d", vim.diagnostic.goto_prev, opts)

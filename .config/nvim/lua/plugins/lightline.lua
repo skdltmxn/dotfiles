46881dd61823e0c54166ef0f105554f0f37277ab
skdltmxn <supershop@naver.com>
2026-03-29 20:15:20 +0900

    fix lightline with catppuccin

--- .config/nvim/lua/plugins/lightline.lua
+++ .config/nvim/lua/plugins/lightline.lua
@@ -1,9 +1,14 @@
 return {
     {
+        "catppuccin/vim",
+        name = "catppuccin-vim",
+        lazy = false,
+    },
+    {
         "itchyny/lightline.vim",
         config = function()
             vim.g.lightline = {
-                colorscheme = "catppuccin",
+                colorscheme = "catppuccin_frappe",
                 active = {
                     left = { { "mode", "paste" }, { "readonly", "absolutepath", "modified" } },
                 }
